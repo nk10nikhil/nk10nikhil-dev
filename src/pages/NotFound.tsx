96fe214d7757974858dc279fd3443a67479e2960
nk10nikhil <nk10nikhil@gmail.com>
2025-04-20 03:07:51 +0530

    Not Found Page Update

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -27,7 +27,7 @@ const NotFound = () => {
       className="bg-transparent min-h-screen relative"
     >
       {/* Background Elements */}
-      {/* <P5Background className="blur-sm" /> */}
+      <P5Background className="blur-sm" />
       <BlurBackground />
       <FloatingObjects />
 
