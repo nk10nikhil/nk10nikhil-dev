c48d86210058962f3b0336043e47e8e7d5abe7bc
nk10nikhil <nk10nikhil@gmail.com>
2025-04-20 03:05:29 +0530

    Project Page Updated

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -8,7 +8,154 @@ import FloatingObjects from "@/components/elements/FloatingObjects";
 import P5Background from "@/components/elements/P5Background";
 import StarOnGithubButton from "@/components/elements/StarOnGithubButton";
 "use client";
-import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
+import { cn } from "@/lib/utils";
+import { useRef, useState, createContext, useContext } from "react";
+import React from "react";
+
+const MouseEnterContext = createContext<
+  [boolean, React.Dispatch<React.SetStateAction<boolean>>] | undefined
+>(undefined);
+
+export const CardItem = ({
+  as,
+  children,
+  className,
+  translateX = 0,
+  translateY = 0,
+  translateZ = 0,
+  rotateX = 0,
+  rotateY = 0,
+  rotateZ = 0,
+  ...rest
+}: {
+  as?: any; // Change React.ElementType to any to fix typing issues
+  children: React.ReactNode;
+  className?: string;
+  translateX?: number | string;
+  translateY?: number | string;
+  translateZ?: number | string;
+  rotateX?: number | string;
+  rotateY?: number | string;
+  rotateZ?: number | string;
+  [key: string]: any;
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [isMouseEntered] = useMouseEnter();
+  const Tag = as || 'div';
+
+  useEffect(() => {
+    handleAnimations();
+  }, [isMouseEntered]);
+
+  const handleAnimations = () => {
+    if (!ref.current) return;
+    if (isMouseEntered) {
+      ref.current.style.transform = `translateX(${translateX}px) translateY(${translateY}px) translateZ(${translateZ}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg)`;
+    } else {
+      ref.current.style.transform = `translateX(0px) translateY(0px) translateZ(0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg)`;
+    }
+  };
+
+  return (
+    <Tag
+      ref={ref}
+      className={cn("w-fit transition duration-200 ease-linear", className)}
+      {...rest}
+    >
+      {children}
+    </Tag>
+  );
+};
+
+export const CardBody = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "h-60 w-40 [transform-style:preserve-3d]  [&>*]:[transform-style:preserve-3d]",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const CardContainer = ({
+  children,
+  className,
+  containerClassName,
+}: {
+  children?: React.ReactNode;
+  className?: string;
+  containerClassName?: string;
+}) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [isMouseEntered, setIsMouseEntered] = useState(false);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!containerRef.current) return;
+    const { left, top, width, height } =
+      containerRef.current.getBoundingClientRect();
+    const x = (e.clientX - left - width / 2) / 25;
+    const y = (e.clientY - top - height / 2) / 25;
+    containerRef.current.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+  };
+
+  const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
+    setIsMouseEntered(true);
+    if (!containerRef.current) return;
+  };
+
+  const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!containerRef.current) return;
+    setIsMouseEntered(false);
+    containerRef.current.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  };
+  return (
+    <MouseEnterContext.Provider value={[isMouseEntered, setIsMouseEntered]}>
+      <div
+        className={cn(
+          "py-5 flex items-center justify-center",
+          containerClassName
+        )}
+        style={{
+          perspective: "2000px",
+        }}
+      >
+        <div
+          ref={containerRef}
+          onMouseEnter={handleMouseEnter}
+          onMouseMove={handleMouseMove}
+          onMouseLeave={handleMouseLeave}
+          className={cn(
+            "flex items-center justify-center relative transition-all duration-200 ease-linear",
+            className
+          )}
+          style={{
+            transformStyle: "preserve-3d",
+          }}
+        >
+          {children}
+        </div>
+      </div>
+    </MouseEnterContext.Provider>
+  );
+};
+
+// Create a hook to use the context
+export const useMouseEnter = () => {
+  const context = useContext(MouseEnterContext);
+  if (context === undefined) {
+    throw new Error("useMouseEnter must be used within a MouseEnterProvider");
+  }
+  return context;
+};
 
 const projects = [
   {
@@ -143,6 +290,90 @@ const projects = [
   }
 ];
 
+export function ProjectCard() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-0 mt-[-20px]">
+      {projects.map((project, index) => (
+        (<CardContainer className="inter-var">
+          <CardBody
+            className="bg-transparent relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-transparent dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[27rem] h-auto rounded-xl p-6 border  ">
+            <CardItem
+              translateZ="50"
+              className="text-xl font-bold text-neutral-600 dark:text-white">
+              {project.title}
+            </CardItem>
+
+            <CardItem
+              as="p"
+              translateZ="60"
+              className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300">
+              {project.description}
+            </CardItem>
+
+            <CardItem translateZ="100" rotateX={3} rotateZ={-3} className="w-full mt-4">
+              <img
+                src={project.image}
+                height="1000"
+                width="1000"
+                className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+                alt={project.title} />
+            </CardItem>
+
+            <div className="flex justify-between items-center">
+              <CardItem
+                translateZ={100}
+                translateX={15}
+                rotateX={5}
+                as="button"
+                className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300 p-6">
+                <div className="flex flex-wrap gap-2">
+                  {project.tags.slice(0, 4).map((tag) => (
+                    <span
+                      key={`${project.id}-${tag}`}
+                      className="text-xs px-2 py-1 rounded-full bg-primary/10 text-purple-200"
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                  {project.tags.length > 4 && (
+                    <span className="text-xs px-2 py-1 rounded-full bg-secondary text-secondary-foreground">
+                      +{project.tags.length - 4}
+                    </span>
+                  )}
+                </div>
+              </CardItem>
+            </div>
+
+            <div className="flex justify-between items-center mt-20">
+
+              <CardItem
+                translateZ={20}
+                translateX={-40}
+                as="planeHelper"
+                className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white bg-gradient-to-br from-primary via-purple-500 to-indigo-400 hover:bg-primary/90">
+                <a href={project.demoLink} target="_blank" rel="noopener noreferrer" className="flex">
+                  Live Demo <ArrowUpRight className="ml-1 h-4 w-4" />
+                </a>
+              </CardItem>
+
+              <CardItem
+                translateZ={20}
+                translateX={40}
+                as="p"
+                className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold">
+                <a href={project.githubLink} target="_blank" rel="noopener noreferrer" className="flex">
+                  <Github className="mr-1 h-4 w-4" /> Code
+                </a>
+              </CardItem>
+
+            </div>
+          </CardBody>
+        </CardContainer>)
+      ))}
+    </div>
+  );
+}
+
 const Projects = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -157,7 +388,7 @@ const Projects = () => {
       className="bg-transparent min-h-screen relative"
     >
       {/* Background Elements */}
-      {/* <P5Background className="blur-sm" /> */}
+      <P5Background className="blur-sm" />
       <BlurBackground />
       <FloatingObjects />
       <Navbar />
@@ -179,7 +410,7 @@ const Projects = () => {
             </span>
           </motion.div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-[-80px]">
+          {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-[-80px]">
             {projects.map((project, index) => (
               <CardContainer>
                 <CardBody>
@@ -255,7 +486,9 @@ const Projects = () => {
                 </CardBody>
               </CardContainer>
             ))}
-          </div>
+          </div> */}
+          <ProjectCard />
+
         </section>
       </main>
       <section className="container mx-auto px-4 md:px-6 pt-12">
